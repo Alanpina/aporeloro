d2701f1c231b0bcad21cf928df81fa2ef76178b0
Alanpina <apastorpina@gmail.com>
2024-02-09 00:37:05 -0600

    Add: Header Branch

--- aporeloro/src/app/components/header/header.component.ts
+++ aporeloro/src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  styleUrls:['./header.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent {}
