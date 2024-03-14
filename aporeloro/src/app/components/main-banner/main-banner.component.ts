823b33f808654485bf0953c4df51da70ff737a7c
Alanpina <apastorpina@gmail.com>
2024-03-14 02:34:23 -0600

    Add: fix main banner text

--- aporeloro/src/app/components/main-banner/main-banner.component.ts
+++ aporeloro/src/app/components/main-banner/main-banner.component.ts
@@ -6,4 +6,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls:['./main-banner.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class MainBannerComponent {}
+
+export class MainBannerComponent {
+  p1= 'Somos un taller de serigrafia especializada en la creación de productos textiles para marcas y artistas. Desde camisetas hata accesorios, nos dedicamos a plamar tus diseños con la más alta claidad y precisión en serigrafia. Además de la serigrafía textil, ofrecemos servicios de diseño de empaques y otras aplicaciones serigraficas para dar vida a tu marca.'
+
+}
