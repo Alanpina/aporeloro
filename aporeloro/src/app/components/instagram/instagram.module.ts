d7ac9a687369a973fe35af8306df66e6127d36b0
Alanpina <apastorpina@gmail.com>
2024-03-13 23:50:12 -0600

    Add: Behold widget

--- aporeloro/src/app/components/instagram/instagram.module.ts
+++ aporeloro/src/app/components/instagram/instagram.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { InstagramComponent } from './instagram.component';
+import { BeholdWidgetComponent } from '@behold/angular';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, BeholdWidgetComponent],
   declarations: [InstagramComponent],
   exports: [InstagramComponent],
 })
