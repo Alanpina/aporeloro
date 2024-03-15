e7b30b57570c87a1521e61b91a0fe461ebc163a5
Alanpina <apastorpina@gmail.com>
2024-03-14 23:38:58 -0600

    Fix: style for store-section when max-width is 767px

--- aporeloro/src/app/components/store-section/store-section.component.ts
+++ aporeloro/src/app/components/store-section/store-section.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './store-section.component.html',
   styleUrls: ['./store-section.component.scss']
 })
-export class StoreSectionComponent {}
+export class StoreSectionComponent {
+  title : string = 'Nuestra Merch'
+  text: string = 'Descubre La mejor selección de productos en nuestra tienda en linea. Haz click ahora para una experiencia rápida y segura. ¡Te esperamos!'
+  textButton: string = 'Entrar'
+}
