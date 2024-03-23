ed6a0613657a170fc5fc6e1f22813e9b6a71cf8c
Alanpina <apastorpina@gmail.com>
2024-03-23 01:29:07 -0600

    fix: instagram feed content size

--- aporeloro/src/app/components/instagram/instagram.component.ts
+++ aporeloro/src/app/components/instagram/instagram.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserverService } from 'src/app/core/breakpoint-observer/breakpoint-observer.service';
 
 @Component({
   selector: 'app-instagram',
   templateUrl: './instagram.component.html',
-  styleUrls: ['./instagram.component.scss']
+  styleUrls: ['./instagram.component.scss'],
 })
-export class InstagramComponent {}
+export class InstagramComponent implements OnInit {
+  mediumFeedToken = 'HesgDnZAPgduIUl3HUW0';
+  largeFeedToken = '6ve5IkJUFkWwu0r7gio8';
+
+  tokenToShow: string =''
+  constructor(private breakPointObserver : BreakpointObserverService) {}
+
+  ngOnInit(): void {
+    this.tokenToShow = this.breakPointObserver.isMediumSize ?   this.mediumFeedToken : this.largeFeedToken
+  }
+}
