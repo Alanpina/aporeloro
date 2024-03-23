36632a43bdbaa26750ceafe11d67e7f11693765e
Alanpina <apastorpina@gmail.com>
2024-03-22 23:59:34 -0600

    Add: footer info

--- aporeloro/src/app/components/footer/footer.module.ts
+++ aporeloro/src/app/components/footer/footer.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { FooterComponent } from './footer.component';
+import { TextLogoModule } from '../text-logo/text-logo.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, TextLogoModule],
   declarations: [FooterComponent],
   exports: [FooterComponent],
 })
