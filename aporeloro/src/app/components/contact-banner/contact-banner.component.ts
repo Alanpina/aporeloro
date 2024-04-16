39898efd3d88521f6d7ed23ad4aba40ed10301e5
Alanpina <apastorpina@gmail.com>
2024-04-16 00:25:52 -0600

    fix: contact banner size for mobile

--- aporeloro/src/app/components/contact-banner/contact-banner.component.ts
+++ aporeloro/src/app/components/contact-banner/contact-banner.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './contact-banner.component.html',
   styleUrl: './contact-banner.component.scss'
 })
-export class ContactBannerComponent {}
+export class ContactBannerComponent {
+
+  titleLine1= 'Cotiza tu'
+  titleLine2= 'producción'
+  text = 'Deja tu correo y te enviaremos información para cotizar. ¡Estamos Ansiosos de ayudarte a lograr tus objetivos!'
+  textButton = 'A POR EL ORO'
+}
