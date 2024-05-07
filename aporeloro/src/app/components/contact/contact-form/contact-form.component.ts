d98bc7ce27b255b70f218cb255982af2a90442d0
Alanpina <apastorpina@gmail.com>
2024-05-06 23:42:49 -0600

    Contact form design

--- aporeloro/src/app/components/contact/contact-form/contact-form.component.ts
+++ aporeloro/src/app/components/contact/contact-form/contact-form.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrl: './contact-form.component.scss',
 })
-export class ContactFormComponent {
-  constructor(){
+export class ContactFormComponent implements OnInit {
+  constructor() {}
+  buttonText: string = 'Enviar';
+  contactForm: FormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.min(3),
+      Validators.max(200),
+    ]),
+    email: new FormControl('', [Validators.email, Validators.max(50)]),
+    message: new FormControl('', [Validators.required, Validators.max(400)]),
+    resolvedCaptcha: new FormControl('', [Validators.required])
+  });
+  ngOnInit(): void {
     console.log()
+  }
+
+  get buttonDisabled(){
+    return this.contactForm.invalid
+  }
+
+
+  resolved(resolvedEvent: any) {
+    this.contactForm.get('resolvedCaptcha')?.setValue(resolvedEvent)
   }
 }
--- aporeloro/src/app/components/contact/contact-form/contact-form.module.ts
+++ aporeloro/src/app/components/contact/contact-form/contact-form.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { ContactFormComponent } from './contact-form.component';
 import { ContactFormRoutingModule } from './contact-form-routing.module';
+import { RecaptchaModule, RecaptchaV3Module } from 'ng-recaptcha';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, ContactFormRoutingModule],
+  imports: [CommonModule, ContactFormRoutingModule, RecaptchaModule, ReactiveFormsModule],
   declarations: [ContactFormComponent],
 })
 export class ContactFormModule {}
