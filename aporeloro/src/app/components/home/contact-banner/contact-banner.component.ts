601eefd2c77390efee12ebd469119b8df0f4e073
Alanpina <apastorpina@gmail.com>
2024-08-20 22:41:13 -0600

    Fix: contact banner

--- aporeloro/src/app/components/home/contact-banner/contact-banner.component.ts
+++ aporeloro/src/app/components/home/contact-banner/contact-banner.component.ts
@@ -1,14 +1,73 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { take } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact-banner',
   templateUrl: './contact-banner.component.html',
-  styleUrl: './contact-banner.component.scss'
+  styleUrl: './contact-banner.component.scss',
 })
 export class ContactBannerComponent {
+  constructor(private httpClient: HttpClient) {}
 
-  titleLine1= 'Cotiza tu'
-  titleLine2= 'producción'
-  text = 'Deja tu correo y te enviaremos información para cotizar. ¡Estamos Ansiosos de ayudarte a lograr tus objetivos!'
-  textButton = 'A POR EL ORO'
+  contactForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.max(50)]),
+  });
+  titleLine1 = 'Cotiza tu';
+  titleLine2 = 'producción';
+  text =
+    'Deja tu correo y te enviaremos información para cotizar. ¡Estamos Ansiosos de ayudarte a lograr tus objetivos!';
+  textButton = 'A POR EL ORO';
+
+  get buttonDisabled() {
+    return this.contactForm.invalid;
+  }
+
+  sendEmail() {
+    Swal.fire({
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading(null);
+      },
+    });
+    //this.httpClient.post('https://aporeloro.com/laravel/public/api/contact-form', this.contactForm.value).pipe(take(1)).subscribe((data)=>{
+    this.httpClient
+      .post('http://localhost:8000/api/contact-form', this.contactForm.value)
+      .pipe(take(1))
+      .subscribe({
+        next: (data) => {
+          Swal.hideLoading();
+          let dataResponse = data as any;
+          if (dataResponse['message'] === 'ok') {
+            Swal.fire({
+              title: 'El correo de contacto ha sido enviado correctamente',
+              icon: 'success',
+              showConfirmButton: false,
+              timer: 3500,
+            });
+          } else {
+            Swal.fire({
+              title: 'Hubo un error al ponerte en contacto',
+              icon: 'error',
+              showConfirmButton: false,
+              timer: 3500,
+            });
+          }
+        },
+        error: () => {
+          Swal.fire({
+            title: 'Hubo un error al ponerte en contacto',
+            icon: 'error',
+            showConfirmButton: false,
+            timer: 3500,
+          });
+        },
+      });
+  }
+
+  resolved(resolvedEvent: any) {
+    this.contactForm.get('resolvedCaptcha')?.setValue(resolvedEvent);
+  }
 }
--- aporeloro/src/app/components/home/contact-banner/contact-banner.module.ts
+++ aporeloro/src/app/components/home/contact-banner/contact-banner.module.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ContactBannerComponent } from './contact-banner.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PrintErrorModule } from 'src/app/core/print-error/print-error.module';
+import { RecaptchaModule } from 'ng-recaptcha';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ContactBannerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, PrintErrorModule, RecaptchaModule, HttpClientModule],
   exports: [ContactBannerComponent],
 })
 export class ContactBannerModule {}
