92c4e38527fa7c42be95fdcb8fb88b703fd6f7fb
Alanpina <apastorpina@gmail.com>
2025-12-16 22:25:07 -0600

    changes 16-12

--- aporeloro/src/app/components/home/store-section/store-section.module.ts
+++ aporeloro/src/app/components/home/store-section/store-section.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { StoreSectionComponent } from './store-section.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [StoreSectionComponent],
   exports: [StoreSectionComponent],
 })
